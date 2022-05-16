6f52736b69d9fac9e36be791311511835cb2ceb9
Volodymyr Romanov <redmag777@gmail.com>
2022-05-16 17:41:42 +0200

    loop basic 1

---
+++ for_loop_basic1.py
@@ -0,0 +1,23 @@
+for x in range(151):
+    print(x)
+for x in range(5, 1005, 5):
+    print(x)
+for x in range(1,101):
+    print(x)
+    if x%5==0:
+        print("Coding")
+    if x%10==0:
+        print("Coding Dojo")
+sum=0        
+for i in range (1, 501, 2):
+    sum=sum+i
+    print(sum)
+for x in range (2018, 0, -4):
+    print(x)
+
+lowNum=10
+highNum=120
+mult=5    
+for x in range(lowNum, highNum):
+    if x%mult==0:
+        print(x)
